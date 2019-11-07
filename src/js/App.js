25160099e29bf9cec1af5cc8673e7a0bb1cfabfb
hegang <alphia@163.com>
2019-11-07 16:03:03 +0800

    extract main page content out of app.js

--- src/js/App.js
+++ src/js/App.js
@@ -1,95 +1,37 @@
 import React from 'react';
 import '../css/App.scss';
 import Logon from "./Logon";
-import LogonPic from "../pic/logon.jpg";
-import withStyles from "@material-ui/core/styles/withStyles";
-import VirticalTabs from './VirticalTabs';
-
-const styles = {
-    banner: {
-        margin: '0 0 0 50px',
-        fontSize: '30px',
-    },
-    bannerRight: {
-        float: 'right',
-        display: 'block'
-    },
-    bannerP: {
-        display: 'inline-block',
-        margin: '30px 30px',
-    },
-    bannerName: {
-        color: 'rgb(0,100,180,0.7)',
-        display: 'inline-block',
-    },
-    background: {
-        height: '100%',
-        backgroundImage: `url(${LogonPic})`,
-        opacity: '0.2',
-        width: '100%',
-        position: 'absolute',
-        right: 0,
-        zIndex: -1,
-    },
-    bannerTitle: {
-        margin: '50px 0 0 0',
-        fontSize: '80px',
-        color: 'rgb(0,100,180,0.7)'
-    },
-    divider: {
-        height: 30,
-        borderBottom: '2px solid',
-        marginBottom: '45px'
-    }
-};
-
+import Main from "./Main";
 
 class App extends React.Component {
 
     constructor(props, context) {
         super(props, context);
         this.state = {
-            hasLogon: false,
+            isStranger: true,
             userName: "wang",
         }
     }
 
     logon = (userName) => () => {
-        this.setState({hasLogon: true, userName: userName})
+        this.setState({isStranger: false, userName: userName})
     };
 
     logout = () => {
-        this.setState({hasLogon: false})
+        this.setState({isStranger: true})
     };
 
     render() {
-        // let userName = this.state.userName;
-        const {classes} = this.props;
-
         return (
             <React.Fragment>
-                {this.state.hasLogon ?
-                    (<div>
-                        <div className={classes.background}/>
-                        <div className={classes.banner}>
-                            <div className={classes.bannerRight}>
-                                <p className={classes.bannerP}>您好，&nbsp;
-                                    <b className={classes.bannerName}>{this.state.userName}</b>
-                                </p>
-                                <p className={classes.bannerP} onClick={this.logout}>登出</p>
-                            </div>
-                            <h2 className={classes.bannerTitle}>Data Federation Shop </h2>
-                        </div>
-
-                        <div className={classes.divider}></div>
-                        <VirticalTabs/>
-                        {/*<div>*/}
-                        {/*    <HospitalSelector suggestions={{label: 'Xijing Hospital'}, {label: 'Jiaoda First Hospital'}}/>*/}
-                        {/*</div>*/}
-
-                    </div>) : <Logon logonHandler={this.logon}/>}
+                {this.state.isStranger
+                    ?
+                    <Logon logonHandler={this.logon}/>
+                    :
+                    <Main userName={this.state.userName} logoutHandler={this.logout}/>
+                }
             </React.Fragment>)
     }
 }
 
-export default withStyles(styles)(App);
+export default App;

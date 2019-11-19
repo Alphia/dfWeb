0d2b9efdf500cb2d9c81389f060da8366a65936d
hegang <alphia@163.com>
2019-11-19 18:13:35 +0800

    add multiple listing card and ui refinement

--- src/js/Listing.js
+++ src/js/Listing.js
@@ -1,17 +1,13 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Paper from "@material-ui/core/Paper";
 import ListingBox from "./ListingBox";
 
 const styles = {
     root: {
         maxWidth: '1180px',
         margin: 'auto',
-        padding: '0'
-    },
-    paper: {
-        margin:'3rem auto',
-        padding: '1.5rem',
+        padding: '0',
+
     },
 };
 
@@ -21,9 +17,7 @@ class Listing extends React.Component {
         const {classes} = this.props;
         return (
             <div className={classes.root}>
-                <Paper className={classes.paper}>
-                    <ListingBox/>
-                </Paper>
+                <ListingBox/>
             </div>
         )
     }
--- src/js/ListingBox.js
+++ src/js/ListingBox.js
@@ -1,17 +1,30 @@
 import React from "react";
 import {listingModel} from "./MockData";
 import ListingCard from "./ListingCard";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-
+const styles = {
+    listRoot: {
+        '& >div': {
+            marginBottom: '1rem',
+        },
+    },
+    resultTip: {
+        fontSize: '1.7rem',
+        margin: 0,
+    }
+};
 
 class ListingBox extends React.Component {
     render() {
-        return(
-            <div >
-                {listingModel.map(model=><ListingCard model={model}/>)}
+        const {classes} = this.props;
+        return (
+            <div className={classes.listRoot}>
+                <p className={classes.resultTip}>23项数据来源为"西京医院"的搜索结果：</p>
+                {listingModel.map(model => <ListingCard model={model}/>)}
             </div>
         )
     }
 }
 
-export default ListingBox;
+export default withStyles(styles)(ListingBox);

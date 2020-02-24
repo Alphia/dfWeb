a17d9b5ec7b8913a410502d65c3c798fe72da043
hegang <alphia@163.com>
2020-02-24 18:27:16 +0800

    the ports

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 const config={
-    heServerUrl: 'http://2y.2ej.com:6022/',
+    heServerUrl: 'http://2y.2ej.com:3302/',
     heFrontUrl: 'http://2y.2ej.com:6021/',
     registerPath: 'register',
     logonPath: 'login',
